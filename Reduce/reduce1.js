7b1db88fe5ec7c05c80625d7e0d552c6c8994b79
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-14 19:52:45 +0600

    array flat with reduce

--- Reduce/reduce1.js
+++ Reduce/reduce1.js
@@ -1,9 +1,19 @@
 const numbers = [1, 2, 3, 4, 5]
 
 function sumFunc(accumulator, currentValue, index, arr) {
-    console.log(`index ${index}, Acc ${accumulator}, Cur ${currentValue}`);
+    // console.log(`index ${index}, Acc ${accumulator}, Cur ${currentValue}`);
     return accumulator + currentValue;
 }
 
 const sum = numbers.reduce(sumFunc, 0)
-console.log(sum);
+// console.log(sum);
+
+let nestedArray = [1, [2, 3], 4, [5, 6, 7]]
+const flatArray = nestedArray.flat();
+// console.log(flatArray);
+
+const flatArray1 = nestedArray.reduce((acc, cur) => {
+    console.log(`acc - ${acc}, cur - ${cur}`);
+    return acc.concat(cur)
+}, [])
+console.log(flatArray1);
