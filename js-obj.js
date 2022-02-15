edc990189a94b44942cf3f659baebbc7d6464f50
Ayyub Iqbal <abuayubiqbal@gmail.com>
2022-02-15 13:22:42 +0600

    js object operation

--- js-obj.js
+++ js-obj.js
@@ -3,13 +3,22 @@ const person = {
     Age: 23,
     Study: 'Diploma in CST',
     hobby: ['Reading', 'Watching', 'Listening', 'Eating', 'Traveling'],
+    action() {
+        this.hobby.forEach(hobby => console.log(`${this.name} is ${hobby} and his age is ${this.Age}`))
+    }
 }
 
-// update property / element
+person.action()
 
+// update property / element
 // dot notation
 person.Age = 25;
-
 // bracket notation
-person['name'] = 'Abu Ayub MD Iqbal'
+person['name'] = 'Abu Ayub MD Iqbal';
+person.education = 'Diploma in Computer Science and Technology'
+
+// delete element
+delete person.Study;
+
+console.log('------------------------');
 console.log(person);
